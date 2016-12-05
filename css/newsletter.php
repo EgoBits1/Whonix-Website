9d2cb5eaac30a898bf169344503608fcaa8b1478
EgoBits1 <egobits1@gmail.com>
2016-12-06 00:03:49 +0100

    Update newsletter.php

--- css/newsletter.php
+++ css/newsletter.php
@@ -1,21 +1,16 @@
 <?php
-## CONFIG ##
-# LIST EMAIL ADDRESS
-$recipient = "egobits1@gmail.com";
-# SUBJECT (Subscribe/Remove)
+
+$recipient = "INSERTADDRESS";
+
 $subject = "Subscribe";
-# RESULT PAGE
+
 $location = "/thanks";
-## FORM VALUES ##
-# SENDER - WE ALSO USE THE RECIPIENT AS SENDER IN THIS SAMPLE
-# DON'T INCLUDE UNFILTERED USER INPUT IN THE MAIL HEADER!
+
 $sender = $recipient;
-# MAIL BODY
-$body .= "Name: ".$_REQUEST['Name']." \n";
+
 $body .= "Email: ".$_REQUEST['Email']." \n";
-# add more fields here if required
-## SEND MESSGAE ##
+
 mail( $recipient, $subject, $body, "From: $sender" ) or die ("Mail could not be sent.");
-## SHOW RESULT PAGE ##
+
 header( "Location: $location" );
 ?>
